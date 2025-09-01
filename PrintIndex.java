bdac63d31bec2d0ab81c5059072477d291f0ae83
Rohit <rockieboi10@gmail.com>
2025-09-01 12:03:45 +0530

    Update PrintIndex.java

--- PrintIndex.java
+++ PrintIndex.java
@@ -1,7 +1,7 @@
 public class PrintIndex {
     public static void main(String[] args) {
         int[] arr = {0, 2, 2, 1, 2, 0, 3, 2, 2, 3, 0, 1, 1, 2, 1, 0};
-        int z = 3; // change this value to find other numbers
+        int z = 3; 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == z) System.out.println("Index: " + i);
         }
