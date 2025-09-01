3802c1b40f2f9e0536e15ffbbf403ef3dcbac4dd
Rohit <rockieboi10@gmail.com>
2025-09-01 12:03:14 +0530

    Update CountOccurrences.java

--- CountOccurrences.java
+++ CountOccurrences.java
@@ -1,7 +1,7 @@
 public class CountOccurrences {
     public static void main(String[] args) {
         int[] arr = {0, 2, 2, 1, 2, 0, 3, 2, 2, 3, 0, 1, 1, 2, 1, 0};
-        int y = 2; // change this value to count other numbers
+        int y = 2; 
         int freq = 0;
         for (int num : arr) {
             if (num == y) freq++;
